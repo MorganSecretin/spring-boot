f473c308061d667ee0e3053b69152232fd69ffba
SolaireFR <jesappellegrootdu59@gmail.com>
2024-11-19 16:50:38 +0100

    OB : Ajout d'action avec la commande ob.

--- src/main/java/ynov/solid/d/solution/App.java
+++ src/main/java/ynov/solid/d/solution/App.java
@@ -7,5 +7,14 @@ public class App {
 
         car = new Car(new ElectricEngine());
         car.start();
+
+        car = new Car(new MagicEngine());
+        car.start();
+    }
+
+    static class MagicEngine implements IEngine {
+        public void start() {
+            System.out.println("Magic engine started.");
+        }
     }
 }
